1a05b70fb6ba98ab375b67fe472ddb4a2ca9efa1
ssauvageau- <smark.sauvageau@gmail.com>
2025-02-02 21:14:30 -0500

    Moderation stuff

--- commands/moderation.py
+++ commands/moderation.py
@@ -2,6 +2,7 @@ from datetime import datetime, timedelta, timezone
 import json
 import logging
 import os
+import asyncio
 from typing import Dict
 
 import discord
@@ -111,3 +112,79 @@ class ModerationCommandGroup(app_commands.Group, name="moderation"):
                 file=discord.File(fn), ephemeral=True
             )
             os.unlink(fn)
+
+    @app_commands.command(
+        name="range-delete",
+        description="Delete all messages in a channel that occur between two supplied messages, inclusive.",
+    )
+    @app_commands.describe(
+        msg_one="ID of the first of the two message block limits, inclusive.",
+        msg_two="ID of the second of the two message block limits, inclusive.",
+    )
+    @app_commands.checks.has_any_role(*COMMAND_ROLE_ALLOW_LIST)
+    async def range_delete(
+        self,
+        interaction: discord.Interaction,
+        msg_one: str,
+        msg_two: str,
+        limit: int = 200,
+    ):
+        chan = interaction.channel
+        m1 = await chan.fetch_message(int(msg_one))
+        m2 = await chan.fetch_message(int(msg_two))
+        if m1 is None or m2 is None:
+            await interaction.response.send_message(
+                "Messages not found in calling channel, terminating operation.\nYou must use this command from the channel that contains both messages.",
+                ephemeral=True,
+            )
+        await interaction.response.defer(ephemeral=True, thinking=True)
+        hist = [message async for message in chan.history(limit=limit)]
+        print(hist)
+        bound1, bound2 = 0, 0
+        for x in range(len(hist)):
+            if hist[x].id == int(msg_one):
+                bound1 = x
+            elif hist[x].id == int(msg_two):
+                bound2 = x
+        if bound1 == 0 or bound2 == 0:
+            await interaction.followup.send(
+                f"Messages not found within history of {limit} messages in channel.",
+                ephemeral=True,
+            )
+        # python list slicing is [x:y) by default, need to add 1 to upper bound
+        deletions = (
+            hist[bound1 : bound2 + 1] if bound1 < bound2 else hist[bound2 : bound1 + 1]
+        )
+        queue = [deletions[i : i + 5] for i in range(0, len(deletions), 5)]
+        for batch in queue:
+            for msg in batch:
+                await msg.delete()
+            await asyncio.sleep(1)
+        await interaction.followup.send(
+            f"Deleted {len(deletions)} messages.", ephemeral=True
+        )
+
+    @app_commands.command(
+        name="get-channel-pos",
+        description="The position in the channel list. This is a number that starts at 0.",
+    )
+    @app_commands.checks.has_any_role(*COMMAND_ROLE_ALLOW_LIST)
+    async def get_channel_position(
+        self, interaction: discord.Interaction, channel: discord.TextChannel
+    ):
+        await interaction.response.send_message(
+            f"{channel.jump_url} is in position {channel.position}", ephemeral=True
+        )
+
+    @app_commands.command(
+        name="move-channel",
+        description="Moves a channel into the specific position. Recommend using the get-channel-pos command first.",
+    )
+    @app_commands.checks.has_any_role(*COMMAND_ROLE_ALLOW_LIST)
+    async def move_channel(
+        self, interaction: discord.Interaction, channel: discord.TextChannel, pos: int
+    ):
+        await channel.edit(position=pos)
+        await interaction.response.send_message(
+            f"{channel.jump_url} moved to position {pos}", ephemeral=True
+        )
