610e19ff20b8665b9c9e65e46ce38d402a62c0c9
ssauvageau- <smark.sauvageau@gmail.com>
2025-08-21 15:25:48 -0400

    Redis fix?

--- bot.py
+++ bot.py
@@ -30,7 +30,9 @@ class DegeneBot(commands.Bot):
         super().__init__(command_prefix="!", intents=intents, activity=activity)
 
     async def setup_hook(self):
-        redis_client = redis.Redis(host="redis", port=6379, decode_responses=False)
+        redis_client = redis.Redis(
+            host="redis_server", port=6379, decode_responses=False
+        )
         try:
             await redis_client.ping()
         except Exception as e:
